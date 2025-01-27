b40eb328f6a655acc59ae72aefbc0570024206e5
DarshD22 <dujrai.darsh0122@gmail.com>
2025-01-27 11:03:16 +0530

    updated

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const connectToMongo = () => {
   mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true, family:4 })
+    .connect(process.env.MONGODB_CONNECTION_STRING)
     .then(() => {
       console.log("Connected to MongoDB");
     })
